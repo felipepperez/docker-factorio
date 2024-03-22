ccdd48f662811d0bff248f0510c8f16facaa9119
felipepperez <fpproducoes@gmail.com>
2024-03-21 22:56:41 -0400

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -8,7 +8,7 @@ echo "Starting Factorio Server..."
 
 # Start the server and track the process ID
 rm ${FACTORIO_PID_FILE} > /dev/null 2>&1
-./factorio/bin/x64/factorio --start-server $(cat last-game.txt) > ${LOG_FILE} 2>&1 < ${FIFO_NAME} &
+./factorio/bin/x64/factorio --start-server $(cat last-game/last-game.txt) > ${LOG_FILE} 2>&1 < ${FIFO_NAME} &
 echo $! > ${FACTORIO_PID_FILE}
 
 echo "Done!"
